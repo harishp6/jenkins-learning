95ec9bf19f009d11e3783a022ffa7f13939962f9
Hareesh Parvathaneni <hparvathaneni@evoketechnologies.com>
2026-08-02 16:47:41 +0530

    fixed the test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
 @Test
 void shouldAddTwoNumbers() {
 Calculator calc = new Calculator();
-int resulst = calc.add(2, 3);
+int result = calc.add(2, 3);
 assertEquals(5, result);
 
 }
